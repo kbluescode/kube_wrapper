50855f09f0d55554d0a858f39357170b400c3f4c
Kevin Blues <kbluescode@gmail.com>
2019-08-27 14:01:00 -0700

    feat(initial commit): Initial commit

---
+++ lib/kube_wrapper.rb
@@ -0,0 +1,6 @@
+require "kube_wrapper/version"
+
+module KubeWrapper
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/kube_wrapper/version.rb
@@ -0,0 +1,3 @@
+module KubeWrapper
+  VERSION = "0.1.0"
+end
